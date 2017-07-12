82438c3660a0489d415148e7fa4ce3681c48f1a6
Lei Jin <303548901@qq.com>
2017-07-12 14:35:30 +0800

    Update alertConfigParser.js

--- web/utils/alertConfigParser.js
+++ web/utils/alertConfigParser.js
@@ -2,7 +2,7 @@
  * Created by King on 16/4/22.
  */
 
-var str = {"id":1,"project_id":1,"project_name":"hawkeye","config":"{\n  \"file_path\": \"/Users/King/Work/code58/hawkeye/client/logs/app.log\",\n  \"chain\": [\n    {\n      \"type\": \"level\",\n      \"value\": \"error\"\n    },{\n      \"type\": \"keyword\",\n      \"value\": \"Exception\"\n    }\n  ],\n  \"alert\": {\n    \"type\": \"blank\",\n    \"value\": \"jinleihao@58ganji.com\"\n  }\n}"};
+var str = {"id":1,"project_id":1,"project_name":"hawkeye","config":"{\n  \"file_path\": \"/Users/King/Work/code58/hawkeye/client/logs/app.log\",\n  \"chain\": [\n    {\n      \"type\": \"level\",\n      \"value\": \"error\"\n    },{\n      \"type\": \"keyword\",\n      \"value\": \"Exception\"\n    }\n  ],\n  \"alert\": {\n    \"type\": \"blank\",\n    \"value\": \"\"\n  }\n}"};
 
 var filter = {
     levelFilter: function (log) {
